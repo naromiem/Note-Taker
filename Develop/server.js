0443b3e520de3cec516e45340d9d9ad8ea2f30c3
naromiem <71422309+naromiem@users.noreply.github.com>
2020-12-17 15:49:30 -0500

    able to take take notes in app

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
 const path = require("path");
+const fs = require("fs");
+
+const noteArray = require("./db/db.json");
 
 const app = express();
 
@@ -8,8 +10,8 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static("public"))
-//const noteArray = [];
+app.use(express.static("public"));
+
 
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "public/notes.html"));
@@ -18,14 +20,25 @@ app.get("/notes", function(req, res) {
 
 
 app.get("/api/notes", function(req, res) {
+  res.json(noteArray);
+  console.log(noteArray)
 
 });
 
 app.post("/api/notes", function(req, res) {
+  const newNote = req.body;
+  newNote ["id"] =
+  noteArray.length;
+  console.log(newNote)
+
+  noteArray.push(newNote);
+  res.json(true);
  
 });
 
 app.delete("/api/notes/:id", function(req, res) {
+  //res.send("testing");
+  //console.log(res)
   
 });
 
